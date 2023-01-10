2a9a663b7ee020d9da092bd90a25def0cacc3cb5
StefaniaSperani <thedarkcry@hotmail.it>
2023-01-10 16:14:06 +0100

    scaffolding + scss

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="jumbotron p-5 mb-4 rounded-3">
+    <div class="container py-5 text-center">
+        <h1 class="display-5 fw-bold">
+            Stefania Sperani: My Portfolio!
+        </h1>
+        <button class="btn btn-danger btn-lg" type="button">Discover</button>
+    </div>
+</div>
+
+<div class="content">
+    <div class="container">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora temporibus, dicta nemo aliquam totam nisi deserunt soluta quas voluptatum ab beatae praesentium necessitatibus minus, facilis illum rerum officiis accusamus dolores!</p>
+    </div>
+</div>
+@endsection
